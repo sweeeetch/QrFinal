34543a5e9c293a3f371178e60345867151f53be4
SAIM ZAIR <103188197+sweeeetch@users.noreply.github.com>
2025-02-22 18:01:05 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCy0QmFRWx3-cvtGkSHlMlgMmLblfTvoo4",
-  authDomain: "bkform-d309e.firebaseapp.com",
-  projectId: "bkform-d309e",
-  storageBucket: "bkform-d309e.firebasestorage.app",
-  messagingSenderId: "691160228613",
-  appId: "1:691160228613:web:1b0330df219ea144ff4d6c"
+  apiKey: "A#",
+  authDomain: "#",
+  projectId: "#",
+  storageBucket: "#",
+  messagingSenderId: "#",
+  appId: "#"
 };
 
 const app = initializeApp(firebaseConfig);
